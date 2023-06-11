01c55339af1ad4d41bc8983e84c6dde3b493508e
Inseok Chang <inni.chang@gmail.com>
2023-06-11 22:15:22 +0900

    ADD: unit test for users

--- src/users/controllers/users/users.controller.spec.ts
+++ src/users/controllers/users/users.controller.spec.ts
@@ -1,8 +1,6 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { UsersController } from './users.controller';
 import { UsersService } from 'src/users/services/users/users.service';
-import { TypeOrmModule } from '@nestjs/typeorm';
-import { User as UserEntity } from 'src/typeorm';
 
 describe('UsersController', () => {
   let controller: UsersController;
@@ -33,7 +31,6 @@ describe('UsersController', () => {
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      imports: [TypeOrmModule.forFeature([UserEntity])],
       controllers: [UsersController],
       providers: [
         {
@@ -54,18 +51,92 @@ describe('UsersController', () => {
     expect(controller).toBeDefined();
   });
 
-  it('should return an array of users', async () => {
-    const result = [
-      {
+  it('it should get all users', async () => {
+    const mockUser = {
+      id: 1,
+      username: 'test',
+      password: 'test',
+      emailAddress: 'test',
+      posts: [],
+    };
+
+    jest.spyOn(service, 'getUsers').mockResolvedValue([mockUser]);
+
+    const result = await controller.getUsers();
+
+    expect(result).toEqual(
+      expect.arrayContaining([
+        expect.objectContaining({
+          emailAddress: expect.any(String),
+          id: expect.any(Number),
+          username: expect.any(String),
+          password: expect.any(String),
+          posts: expect.any(Array),
+        }),
+      ]),
+    );
+  }),
+    it('it should get user by name', async () => {
+      const mockUser = {
         id: 1,
         username: 'test_username',
-        password: 'test_password',
-        emailAddress: 'test@email.com',
+        emailAddress: 'test@gmail.com',
+        password: 'testpassword',
         posts: [],
-      },
-    ];
-    jest.spyOn(service, 'getUsers').mockResolvedValue(result);
+      };
 
-    expect(await controller.getUsers()).toEqual(result);
-  });
+      jest.spyOn(service, 'getUserByName').mockResolvedValue(mockUser);
+
+      const result = await controller.getUsersByName('test_username');
+
+      expect(result).toEqual(
+        expect.objectContaining({
+          id: expect.any(Number),
+          username: expect.any(String),
+          emailAddress: expect.any(String),
+        }),
+      );
+    }),
+    it('it should get user by id', async () => {
+      const mockUser = {
+        id: 1,
+        username: 'test_username',
+        emailAddress: 'test@gmail.com',
+        password: 'testpassword',
+        posts: [],
+      };
+
+      jest.spyOn(service, 'getUserById').mockResolvedValue(mockUser);
+
+      const result = await controller.getUserById(1);
+
+      expect(result).toEqual(
+        expect.objectContaining({
+          id: expect.any(Number),
+          username: expect.any(String),
+          emailAddress: expect.any(String),
+        }),
+      );
+    }),
+    it('it should return created user', async () => {
+      const mockUser = {
+        id: 1,
+        username: 'test_username',
+        emailAddress: 'test@gmail.com',
+        password: 'testpassword',
+        posts: [],
+      };
+
+      jest.spyOn(service, 'createUser').mockResolvedValue(mockUser);
+
+      const result = await controller.createUser(mockUser);
+
+      expect(result).toEqual(
+        expect.objectContaining({
+          id: expect.any(Number),
+          username: expect.any(String),
+          emailAddress: expect.any(String),
+        }),
+      );
+    });
 });
